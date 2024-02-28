c7cb8183af7d61cafb212ac53e42b10148d58827
Kaiyrkhan Kairolla <111349192+sogeking7@users.noreply.github.com>
2024-02-29 01:16:45 +0600

    add db

--- api/dist/server.js
+++ api/dist/server.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -18,9 +9,7 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const compression_1 = __importDefault(require("compression"));
 const cors_1 = __importDefault(require("cors"));
-const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const router_1 = __importDefault(require("./router"));
 dotenv_1.default.config();
 const port = process.env.PORT || 3000;
 const app = (0, express_1.default)();
@@ -34,17 +23,17 @@ const server = http_1.default.createServer(app);
 server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}/`);
 });
-const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const conn = yield mongoose_1.default.connect(process.env.DATABASE_URL);
-        console.log(`MongoDB connected to ${conn.connection.host}`);
-    }
-    catch (error) {
-        console.error(`MongoDB connection error: ${error.message}`);
-        process.exit(1);
-    }
-});
-connectDB();
-app.use("/", (0, router_1.default)());
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.DATABASE_URL);
+//     console.log(`MongoDB connected to ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error(`MongoDB connection error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+// connectDB();
+// app.use("/", router());
+app.use('/api', (req, res) => res.send('Hello World'));
 exports.default = app;
 //# sourceMappingURL=server.js.map
