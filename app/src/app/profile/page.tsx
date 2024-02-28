e6dc1f5f2b74b9a33d430dcec96e0a5a65bcd4bf
Kaiyrkhan Kairolla <111349192+sogeking7@users.noreply.github.com>
2024-02-29 01:42:13 +0600

    fix: nextjs error

--- app/src/app/profile/page.tsx
+++ app/src/app/profile/page.tsx
@@ -8,24 +8,28 @@ export default function Home() {
   const router = useRouter();
   const { user, resetUser } = useUserStore();
 
-  if (!user) {
-    router.push("/");
-    return;
-  }
+  // if (!user) {
+  //   router.push("/");
+  //   return;
+  // }
 
   return (
     <main className="max-w-3xl mx-auto p-6">
-      <div className="inline">
-        <span className="text-3xl font-semibold">
-          Welcome, {user.username} 👋
-        </span>
-        <div className="mt-6">
-          <Button onClick={() => resetUser()}>Logout</Button>
-        </div>
-        <hr className="my-6"></hr>
-        <h1 className="text-2xl font-semibold">History</h1>
-        <Attempts />
-      </div>
+      {user && (
+        <>
+          <div className="inline">
+            <span className="text-3xl font-semibold">
+              Welcome, {user?.username} 👋
+            </span>
+            <div className="mt-6">
+              <Button onClick={() => resetUser()}>Logout</Button>
+            </div>
+            <hr className="my-6"></hr>
+            <h1 className="text-2xl font-semibold">History</h1>
+            <Attempts />
+          </div>
+        </>
+      )}
     </main>
   );
 }
